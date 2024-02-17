ef1441b239489e28e7ca902bf84184c013cd497f
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2024-02-17 23:09:05 +0530

    Add eventTopic and upcomingevents to schemaTypes

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,5 +2,7 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import eventTopic from './eventTopic'
+import upcomingevents from './events'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, author, category, blockContent, eventTopic, upcomingevents]
